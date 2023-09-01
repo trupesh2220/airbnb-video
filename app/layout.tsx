5e04a2a5ac53baf297df00163c0c7a1b9d91839a
TRUPESH PADSHALA <123795991+trupesh2220@users.noreply.github.com>
2023-09-01 13:17:50 +0530

    One November Task Is complited

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
-import './globals.css'
-
+import Navbar from "./components/Navbar/Nabar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+const font = Nunito({
+  subsets: ["latin"],
+});
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +20,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        </body>
     </html>
-  )
+  );
 }
