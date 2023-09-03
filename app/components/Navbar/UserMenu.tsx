f97f20056a218117eced8a3407e5904911ee60db
TRUPESH PADSHALA <123795991+trupesh2220@users.noreply.github.com>
2023-09-03 15:30:31 +0530

    Fiexd Register Model

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModel from "@/app/hooks/useRegisterModel";
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const registerModel = useRegisterModel();
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
@@ -32,7 +34,7 @@ const UserMenu = () => {
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <MenuItem label="Login" onClick={() => {}} />
-            <MenuItem label="Register" onClick={() => {}} />
+            <MenuItem label="Register" onClick={registerModel.onOpen} />
           </div>
         </div>
       )}
