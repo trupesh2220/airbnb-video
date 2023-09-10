13d78ab3b57cbb9ea947ad9bfe73dca1f7f9e0be
TRUPESH PADSHALA <123795991+trupesh2220@users.noreply.github.com>
2023-09-10 16:28:47 +0530

    input is set successfull

--- app/components/models/RegisterModel.tsx
+++ app/components/models/RegisterModel.tsx
@@ -42,7 +42,9 @@ const RegisterModel = () => {
   const bodyContent =(
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Crate An Account" center/>
-      <Input></Input>
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required></Input>
+      <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required></Input>
+      <Input id="password" label="Password" disabled={isLoading} register={register} errors={errors} required type="password"></Input>
     </div>
   )
   return (
